e8289f8409ef0705ae0d22e90e8dd4c2f4d4e5bb
Administrator <Administrator@USER-PC>
2019-02-13 11:59:01 +0900

    StringType 수정

--- src/data/StringType.java
+++ src/data/StringType.java
@@ -6,6 +6,10 @@ public class StringType {
 		
 		String str = "123";
 		System.out.println(str+123);
+		String str2 = new String("123");
+		System.out.println(str.equals(str2));
+		
+		//str.equals(str2) : str과 str2가 보는 '값'이 같은지
 		
 		//숫자라고 해도 문자가 더해지면 무조건 문자가 됨.
 		
