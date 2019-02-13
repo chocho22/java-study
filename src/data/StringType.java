5f3eecf476e6bfade78983402eb54059eacf60fe
Administrator <Administrator@USER-PC>
2019-02-13 11:35:08 +0900

    첫 자바프로젝트 깃 연동

---
+++ src/data/StringType.java
@@ -0,0 +1,17 @@
+package data;
+
+public class StringType {
+
+	public static void main(String[] args) {
+		
+		String str = "123";
+		System.out.println(str+123);
+		
+		//숫자라고 해도 문자가 더해지면 무조건 문자가 됨.
+		
+		/*
+		 * GIT - 메일같은 서비스
+		 * github - 메일서비스를 제공해주는 회사같은거
+		 */
+	}
+}
